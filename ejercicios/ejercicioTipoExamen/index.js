524167f7490b50344d76bc22c903cb44ca637ed7
Vespertino <Vespertino@C06PC28.informatica.comercio>
2022-12-15 19:33:47 +0100

    a

--- ejercicios/ejercicioTipoExamen/index.js
+++ ejercicios/ejercicioTipoExamen/index.js
@@ -11,43 +11,63 @@ addEventListener("DOMContentLoaded", () => {
     let imagen = document.getElementById("lul");
 
     boton1.addEventListener("click", () => {
-        imagen.classList.remove("animacion");
-        window.requestAnimationFrame(function () {
-            imagen.classList.add("animacion");
-        });
+        remove(imagen)
+        imagen.classList.add("saltar");
+        setTimeout(() => {
+            imagen.classList.remove("saltar");
+        }, 700);
     });
 
     boton2.addEventListener("click", () => {
-        imagen.style.transform = "translateX(-70px) ";
-        imagen.style.transition = "ease-in-out 0.5s";
+        remove(imagen)
+        imagen.style.marginTop = "210px";
+        imagen.classList.add("izquierdaA1");
+        setTimeout(() => {
+            //imagen.classList.remove("izquierdaA");
+        }, 3000);
     });
 
     boton3.addEventListener("click", () => {
-        imagen.style.transform = "translateX(70px) ";
-        imagen.style.transition = "ease-in 0.3s";
+        imagen.style.marginTop = "210px";
+        imagen.classList.add("derechaA1");
+        setTimeout(() => {
+            // imagen.classList.remove("derechaA1");
+
+        }, 2500);
     });
 
     boton4.addEventListener("click", () => {
-        imagen.style.transform = "translateY(50px)";
-        imagen.style.transition = "ease-in 0.3s";
+        remove(imagen)
+        imagen.classList.add("abajo");
+        setTimeout(() => {
+            imagen.classList.remove("abajo");
+            imagen.classList.add("flotar");
+        }, 1000);
     });
 
     botonA.addEventListener("click", () => {
-        imagen.style.transform = "scale(0.5)";
+        remove(imagen)
+        imagen.style.transform = "scale(0.7)";
         imagen.style.transition = "ease-in 0.2s";
+        imagen.style.marginTop = "215px";
     });
 
     botonB.addEventListener("click", () => {
+        remove(imagen)
         imagen.style.transform = "scale(1.5)";
         imagen.style.transition = "ease-in 0.2s";
     });
 
     botonC.addEventListener("click", () => {
+        remove(imagen)
         imagen.style.transform = "translateY(0)";
         imagen.style.transform = "translateX(0)";
         imagen.style.transition = "ease-in 0.3s";
+        imagen.style.marginTop = "210px";
     });
-
-
 });
+
+function remove(imagen) {
+    imagen.classList.remove(...imagen.classList);
+}
 
