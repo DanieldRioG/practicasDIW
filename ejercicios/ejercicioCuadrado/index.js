e68f927e17c229a8bae9467073fb31228158955c
Vespertino <Vespertino@C06PC28.informatica.comercio>
2022-12-16 18:42:40 +0100

    a

--- ejercicios/ejercicioCuadrado/index.js
+++ ejercicios/ejercicioCuadrado/index.js
@@ -18,40 +18,23 @@ addEventListener("DOMContentLoaded", () => {
         contenedor.style.transform = "scale(1.1)";
         contenedor.style.transform = "translateX(" + posI + "px) translateX(" + posD + "px) translateY(" + posA + "px)";
         contenedor.style.transition = "0.3s ease-in-out";
-        cambioCol(posA, posD, posI, circulo)
     });
 
     cuadradoIzquierda.addEventListener("mouseover", () => {
         posI = posI + 50;
         contenedor.style.transform = "translateX(" + posI + "px) translateY(" + posA + "px) translateX(" + posD + "px)";
         contenedor.style.transition = "0.3s ease-in-out";
-        cambioCol(posA, posD, posI, circulo)
-        console.log(posI);
     });
 
     cuadradoDerecha.addEventListener("mouseover", () => {
         posD = posD - 50;
         contenedor.style.transform = "translateX(" + posD + "px) translateY(" + posA + "px) translateX(" + posI + "px)";
         contenedor.style.transition = "0.3s ease-in-out";
-        cambioCol(posA, posD, posI, circulo);
     });
 
     cuadradoAbajo.addEventListener("mouseover", () => {
         posA = posA - 50;
         contenedor.style.transform = "translateX(" + posI + "px) translateX(" + posD + "px) translateY(" + posA + "px)";
         contenedor.style.transition = "0.3s ease-in-out";
-        cambioCol(posA, posD, posI, circulo)
     });
 });
-
-function cambioCol(posA, posD, posI, circulo) {
-    if (posA >= 400 && posA <= 600) {
-        if (posI >= 500 && posI <= 600) {
-            circulo.style.backgroundColor = "green";
-        } else {
-            circulo.style.backgroundColor = "blueviolet";
-        }
-    } else {
-        circulo.style.backgroundColor = "blueviolet";
-    }
-}
--- ejercicios/ejercicioTipoExamen/index.js
+++ ejercicios/ejercicioTipoExamen/index.js
@@ -4,15 +4,21 @@ addEventListener("DOMContentLoaded", () => {
     let boton3 = document.getElementById("derecha");
     let boton4 = document.getElementById("abajo");
 
-    let botonA = document.getElementById("a");
-    let botonB = document.getElementById("b");
-    let botonC = document.getElementById("c");
+    let mini = document.getElementById("a");
+    let mega = document.getElementById("b");
+    let reset = document.getElementById("c");
+    let volar = document.getElementById("d");
+    let nuke = document.getElementById("e");
 
-    let imagen = document.getElementById("lul");
+    let imagen = document.getElementById("pj");
+    let imagen2 = document.getElementById("nuke");
+
+    imagen.classList.add("imagen");
 
     boton1.addEventListener("click", () => {
         remove(imagen)
         imagen.classList.add("saltar");
+        imagen.classList.add("imagen");
         setTimeout(() => {
             imagen.classList.remove("saltar");
         }, 700);
@@ -22,6 +28,7 @@ addEventListener("DOMContentLoaded", () => {
         remove(imagen)
         imagen.style.marginTop = "217px";
         imagen.classList.add("izquierdaA1");
+        imagen.classList.add("imagen");
         setTimeout(() => {
             //imagen.classList.remove("izquierdaA");
         }, 3000);
@@ -30,6 +37,7 @@ addEventListener("DOMContentLoaded", () => {
     boton3.addEventListener("click", () => {
         imagen.style.marginTop = "217px";
         imagen.classList.add("derechaA1");
+        imagen.classList.add("imagen");
         setTimeout(() => {
             // imagen.classList.remove("derechaA1");
 
@@ -37,34 +45,60 @@ addEventListener("DOMContentLoaded", () => {
     });
 
     boton4.addEventListener("click", () => {
-        remove(imagen)
+        remove(imagen);
         imagen.classList.add("abajo");
+        imagen.classList.add("imagen");
         setTimeout(() => {
             imagen.classList.remove("abajo");
             imagen.classList.add("flotar");
         }, 1000);
     });
 
-    botonA.addEventListener("click", () => {
-        remove(imagen)
+    mini.addEventListener("click", () => {
+        remove(imagen);
         imagen.style.transform = "scale(0.7)";
         imagen.style.transition = "ease-in 0.2s";
         imagen.style.marginTop = "220px";
     });
 
-    botonB.addEventListener("click", () => {
-        remove(imagen)
+    mega.addEventListener("click", () => {
+        remove(imagen);
         imagen.style.transform = "scale(1.5)";
         imagen.style.transition = "ease-in 0.2s";
         imagen.style.marginTop = "210px";
     });
 
-    botonC.addEventListener("click", () => {
-        remove(imagen)
+    reset.addEventListener("click", () => {
+        imagen.src = "guide.png";
+        remove(imagen);
+        imagen.classList.add("imagen");
         imagen.style.transform = "translateY(0)";
         imagen.style.transform = "translateX(0)";
-        imagen.style.transition = "ease-in 0.3s";
         imagen.style.marginTop = "217px";
+
+    });
+
+    volar.addEventListener("click", () => {
+        remove(imagen);
+        imagen.src = "glide1.png";
+        imagen.classList.add("imagen2");
+        imagen.style.marginTop = "217px";
+        imagen.classList.add("volar");
+        setTimeout(() => {
+            imagen.src = "glide2.png";
+            imagen.classList.add("imagen3");
+            imagen.classList.add("mantenerVuelo");
+            setTimeout(() => {
+            }, 5000);
+        }, 1000);
+    });
+
+    nuke.addEventListener("click", () => {
+        remove(imagen);
+        imagen2.classList.add("tirarNuke");
+        setTimeout(() => {
+
+        }, 1500);
     });
 });
 
